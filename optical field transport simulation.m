f14968a11e4bee921e6af560bcbfd8bc257a4f31
zhou-zhk <168071742+zhou-zhk@users.noreply.github.com>
2024-05-13 00:18:28 +0800

    Update optical field transport simulation.m

--- optical field transport simulation.m
+++ optical field transport simulation.m
@@ -4,7 +4,7 @@ N = 1000;  % pixles
 
 x = linspace(-l/2, l/2, N);
 y = linspace(-l/2, l/2, N);
-[X,Y] = meshgrid(x,y);
+[X,Y] = meshgrid(x,y);  % coordinates of image plane
 
 u = exp(-(X.^2 + Y.^2)/(2*(0.01)^2));  % intensity of the optical field
 U = fftshift(fft2(u));  % Fourier transform 
@@ -14,3 +14,5 @@ dx = x(2) - x(1);
 fx = linspace(-1/(2*dx), 1/(2*dx), N);
 [Fx, Fy] = meshgrid(fx, fx);
 
+D = exp(1i*pi*wavelength*(Fx.^2 + Fy.^2));  % diffraction function
+out = ifft2(ifftshift(U.*D));  % optical amplitude of image plane
