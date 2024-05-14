347b44536e9cc78b0f8c5209fc9d325d0afd0ede
zhou-zhk <168071742+zhou-zhk@users.noreply.github.com>
2024-05-14 15:17:37 +0800

    Update optical field transport simulation.m

--- optical field transport simulation.m
+++ optical field transport simulation.m
@@ -1,6 +1,6 @@
-wavelength = 800e-9;
+wavelength = 800e-9;  % the wavelength of the optical field
 l = 0.05;  % size of image plane
-N = 1000;  % pixles
+N = 1001;  % pixles
 
 x = linspace(-l/2, l/2, N);
 y = linspace(-l/2, l/2, N);
@@ -16,3 +16,13 @@ fx = linspace(-1/(2*dx), 1/(2*dx), N);
 
 D = exp(1i*pi*wavelength*(Fx.^2 + Fy.^2));  % diffraction function
 out = ifft2(ifftshift(U.*D));  % optical amplitude of image plane
+
+% sketch the result of the Fourier transform of the optical beam
+figure;
+imagesc(abs(out2).^2);
+colorbar;axis image;
+title('image plane');
+figure;
+imagesc(u);
+colorbar;axis image;
+title('object plane');
