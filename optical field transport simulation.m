b3c8682d2cb5fe7f70f7ae233b571e22d33b27d1
zhou-zhk <168071742+zhou-zhk@users.noreply.github.com>
2024-05-15 14:08:11 +0800

    Update optical field transport simulation.m

--- optical field transport simulation.m
+++ optical field transport simulation.m
@@ -1,21 +1,21 @@
-wavelength = 800e-9;                        % the wavelength of the optical field
-l = 0.05;                                   % size of image plane
-N = 1001;                                   % pixle number
+wavelength = 800e-9;                          % the wavelength of the optical field
+l = 0.05;                                     % size of image plane
+N = 1001;                                     % pixle number
 
 x = linspace(-l/2, l/2, N);
 y = linspace(-l/2, l/2, N);
-[X,Y] = meshgrid(x,y);                      % coordinates of image plane
+[X,Y] = meshgrid(x,y);                        % coordinates of image plane
 
-u = exp(-(X.^2 + Y.^2)/(2*(0.01)^2));       % intensity of the optical field
-U = fftshift(fft2(u));                      % Fourier transform 
+u = exp(-(X.^2 + Y.^2)/(2*(0.01)^2));         % intensity of the optical field
+U = fftshift(fft2(u));                        % Fourier transform 
 
 % coordinates of frequency domain
 dx = x(2) - x(1);
 fx = linspace(-1/(2*dx), 1/(2*dx), N);
 [Fx, Fy] = meshgrid(fx, fx);
 
-D = exp(1i*pi*wavelength*(Fx.^2 + Fy.^2));  % diffraction function
-out = ifft2(ifftshift(U.*D));               % optical amplitude of image plane
+D = exp(1i*pi*wavelength*(Fx.^2 + Fy.^2));    % diffraction function
+out = ifft2(ifftshift(U.*D));                 % optical amplitude of image plane
 
 % sketch the result of the Fourier transform of the optical beam
 figure;
