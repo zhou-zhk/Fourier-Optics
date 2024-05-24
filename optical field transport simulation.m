dc397193dcc8abaf95cacfd6101a4ad2101f9d11
zhou-zhk <168071742+zhou-zhk@users.noreply.github.com>
2024-05-24 23:44:00 +0800

    Update optical field transport simulation.m

--- optical field transport simulation.m
+++ optical field transport simulation.m
@@ -1,6 +1,6 @@
 wavelength = 800e-9;                          % the wavelength of the optical field
 l = 0.16;                                     % size of image plane
-N = 2001;                                     % pixle number, it should be odd instead of even because of the Fourier algorithm
+N = 2001;                                     % the pixle number which should be odd instead of even because of the Fourier algorithm
 
 x = linspace(-l/2, l/2, N);
 y = linspace(-l/2, l/2, N);
